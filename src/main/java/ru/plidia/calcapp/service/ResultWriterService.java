2ae4981e90922fc28391b2e0a4db4e6066405c0e
P-Lidia <Lydiasmolyakova@gmail.com>
2024-10-03 17:38:50 +0300

    Second commit

--- src/main/java/ru/plidia/calcapp/service/ResultWriterService.java
+++ src/main/java/ru/plidia/calcapp/service/ResultWriterService.java
@@ -2,6 +2,6 @@ package ru.plidia.calcapp.service;
 
 public class ResultWriterService {
     public static void print(int num1, int num2, int result, String name){
-        System.out.println(name + num1 + num2 + result);
+        System.out.println(name + " " + num1 + " " + num2 + " " + result);
     }
 }
