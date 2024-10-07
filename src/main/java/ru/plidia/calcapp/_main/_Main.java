f882332402c2ef89d3f544111d43d02b688bbed2
P-Lidia <Lydiasmolyakova@gmail.com>
2024-10-07 22:56:26 +0300

    Moved the call to the printout method from Calculator class to the Main class. Formatted code.

--- src/main/java/ru/plidia/calcapp/_main/_Main.java
+++ src/main/java/ru/plidia/calcapp/_main/_Main.java
@@ -1,19 +1,26 @@
 package ru.plidia.calcapp._main;
 
+import ru.plidia.calcapp.service.ResultWriterService;
 import ru.plidia.calcapp.util.Calculator;
 
 import java.util.Scanner;
 
 public class _Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите первое число");
         int var1 = sc.nextInt();
         System.out.println("Введите второе число");
         int var2 = sc.nextInt();
-        Calculator.add(var1, var2);
-        Calculator.subtract(var1, var2);
-        Calculator.multiply(var1, var2);
+        int res1 = Calculator.add(var1, var2);
+        String variable1 = "Сумма:";
+        ResultWriterService.print(var1, var2, res1, variable1, "+");
+        int res2 = Calculator.subtract(var1, var2);
+        String variable2 = "Разность:";
+        ResultWriterService.print(var1, var2, res2, variable2, "-");
+        int res3 = Calculator.multiply(var1, var2);
+        String variable3 = "Произведение:";
+        ResultWriterService.print(var1, var2, res3, variable3, "*");
     }
 }
 
--- src/main/java/ru/plidia/calcapp/util/Calculator.java
+++ src/main/java/ru/plidia/calcapp/util/Calculator.java
@@ -1,21 +1,20 @@
 package ru.plidia.calcapp.util;
 
-import ru.plidia.calcapp.service.ResultWriterService;
-
 public class Calculator {
-    public static void add(int num1, int num2){
+    public static int add(int num1, int num2) {
         int sum = num1 + num2;
-        String variable = "Сумма";
-        ResultWriterService.print(num1, num2, sum, variable);
+        return sum;
     }
-    public static void subtract(int num1, int num2){
+
+
+    public static int subtract(int num1, int num2) {
         int minus = num1 - num2;
-        String variable = "Разность";
-        ResultWriterService.print(num1, num2, minus, variable);
+        return minus;
     }
-    public static void multiply(int num1, int num2) {
+
+
+    public static int multiply(int num1, int num2) {
         int multiply = num1 * num2;
-        String variable = "Произведение";
-        ResultWriterService.print(num1, num2, multiply, variable);
+        return multiply;
     }
 }
