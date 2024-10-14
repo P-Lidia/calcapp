df57a6f9134eb1a458d5fa7c13b721bbe53f150b
P-Lidia <Lydiasmolyakova@gmail.com>
2024-10-14 18:56:50 +0300

    fix Calculator

--- src/main/java/ru/plidia/calcapp/_main/_Main.java
+++ src/main/java/ru/plidia/calcapp/_main/_Main.java
@@ -9,18 +9,18 @@ public class _Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите первое число");
-        int var1 = sc.nextInt();
+        int number1 = sc.nextInt();
         System.out.println("Введите второе число");
-        int var2 = sc.nextInt();
-        int res1 = Calculator.add(var1, var2);
+        int number2 = sc.nextInt();
+        int result = Calculator.add(number1, number2);
         String variable1 = "Сумма:";
-        ResultWriterService.print(var1, var2, res1, variable1, "+");
-        int res2 = Calculator.subtract(var1, var2);
+        ResultWriterService.print(number1, number2, result, variable1, "+");
+        result = Calculator.subtract(number1, number2);
         String variable2 = "Разность:";
-        ResultWriterService.print(var1, var2, res2, variable2, "-");
-        int res3 = Calculator.multiply(var1, var2);
+        ResultWriterService.print(number1, number2, result, variable2, "-");
+        result = Calculator.multiply(number1, number2);
         String variable3 = "Произведение:";
-        ResultWriterService.print(var1, var2, res3, variable3, "*");
+        ResultWriterService.print(number1, number2, result, variable3, "*");
     }
 }
 
--- src/main/java/ru/plidia/calcapp/util/Calculator.java
+++ src/main/java/ru/plidia/calcapp/util/Calculator.java
@@ -2,19 +2,14 @@ package ru.plidia.calcapp.util;
 
 public class Calculator {
     public static int add(int num1, int num2) {
-        int sum = num1 + num2;
-        return sum;
+       return num1 + num2;
     }
-
 
     public static int subtract(int num1, int num2) {
-        int minus = num1 - num2;
-        return minus;
+        return num1 - num2;
     }
 
-
     public static int multiply(int num1, int num2) {
-        int multiply = num1 * num2;
-        return multiply;
+       return num1 * num2;
     }
 }
